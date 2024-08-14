1ee4a04b142054d2d14b6d38ebf6c3d08deb60df
Md.Imdadul Haque <imdadhaque2210@gmail.com>
2024-08-14 15:11:31 +0600

    Category Complete V-1

--- src/main/java/com/imdad/cakestroy/controller/AdminController.java
+++ src/main/java/com/imdad/cakestroy/controller/AdminController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -23,17 +26,31 @@ public class AdminController {
     model.addAttribute("categories", categoryService.getAllCategories());
     return "categories";
 }
-
+//ADding Category
 @GetMapping("/admin/categories/add")
     public String getCatAdd(Model model){
     model.addAttribute("category",new Category());
     return "categoriesAdd";
 }
+
     @PostMapping("/admin/categories/add")
     public String postCatAdd(@ModelAttribute("category") Category category){
         categoryService.addCategory(category);
         return "redirect:/admin/categories";
     }
-
+//Deleteing and Updating category
+    @GetMapping("/admin/categories/delete/{id}")
+    public String deleteCat(@PathVariable int id){
+        categoryService.removeCategoryById(id);
+        return "redirect:/admin/categories";
+    }
+    @GetMapping("/admin/categories/update/{id}")
+    public String updateCat(@PathVariable int id, Model model){
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if(category.isPresent()){
+            model.addAttribute("category", category.get());
+            return "categoriesAdd";
+        }else return "404";
+    }
 }
 
